a691f0c85b31880b175fce6c09cb75c724430079
Jason <jasxavier7@gmail.com>
2023-10-06 18:30:58 +1300

    sorted out the back button for the pdfviewer

--- src/components/PdfViewer/PdfViewer.tsx
+++ src/components/PdfViewer/PdfViewer.tsx
@@ -14,8 +14,10 @@ function PdfViewer({hidePdf}: Props) {
 
     return (
       <div className={styles.viewer}>
+        <div className={styles.backButton} onClick={handleButtonClick} title="*professional experiance in Comic Sans*">
+          {"<< CLICK HERE TO GO BACK"}
+        </div>
         <iframe className={styles.iframe} src={pdfUrl} title="PDF Viewer" />
-        <button className={styles.backButton} onClick={handleButtonClick}>BACK</button>
       </div>
     );
 }
