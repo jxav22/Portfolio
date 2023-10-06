ec4881adfc08e9b7698d97c470dfac82396fb80a
Jason <jasxavier7@gmail.com>
2023-10-07 00:43:05 +1300

    added google analytics integration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FTJEG5YXS6"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-FTJEG5YXS6');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
